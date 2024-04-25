8c8ed74c0914e24506e7c349b3311dccaf693919
samanthaanchang <samanthaanchang@gmail.com>
2024-04-24 21:18:53 -0700

    Lab 5 ish
    - partial lab 5
    - links updated for all pages 1-6

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab4: Data Types and Variables
+// Author: Samantha Chang
+// Date: 04/2/2024
 
-// Constants
+// Define Variables
+make = "Toyota";
+model = "Corolla";
+color = "White";
+year = 2021;
 
-// Functions
+// Calculate
+age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+console.log(age)
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
