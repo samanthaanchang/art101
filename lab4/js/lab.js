390063b11725ce949c91ca87be52fac0ab72dadd
samanthaanchang <samanthaanchang@gmail.com>
2024-04-24 19:35:33 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab4/js/lab.js - Creating a Digital Artwork on iPad
+// Author: Samantha Chang <schang79@ucsc.edu>
+// Date: 04/22/2024
+
+//Task 2: Pseudocode a simple computer game (Checkers)
+  //Print the board before every turn.
+	  //>Make the tiles of the board alternating colors, red and black.
+	  //The system will consist of two players, user input, and computer intelligence.
+	//The system asks user for input.
+	  //The user will be able to move their pieces diagonally.
+	    //When user mouse is hovering over diagonal space of a checker, light up the box.
+  //Accept user input.
+    //Validate user input.
+  //Evaluate possible moves.
+  //Computer intelligence makes a move.
+  //Test win, lose, or draw conditions.
 
 // Constants
 
